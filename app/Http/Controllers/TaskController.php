4939c0e56fa26625b1b7102de4982585b7f96e44
Jorge Torres <jotorres060@gmail.com>
2022-05-17 22:20:27 -0500

    TFG

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return response()->json(['data' => $tasks], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string'],
+            'state' => ['required', 'string']
+        ]);
+
+        $task = Task::create($request->only(['name', 'state']));
+
+        return response()->json(['data' => $task], 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => ['required', 'string'],
+            'state' => ['required', 'string']
+        ]);
+        
+        $task = Task::find($id);
+        $task->update(['name' => $request->name, 'state' => $request->state]);
+
+        return response()->json(['data' => $task], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Task::destroy($id);
+
+        return response()->json([], 200);
+    }
+}
